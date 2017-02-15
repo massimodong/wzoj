d7dbfb147423781e223a5a16208b27d6e79ae1b5
Massimo Dong <dongmassimo@gmail.com>
2017-02-15 14:06:56 +0800

    ugly home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,15 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
 	public function index(){
-		return view('home');
+		$recent_problemsets = \App\Problemset::orderBy('updated_at', 'desc')->take(6)->get();
+		$home_page_problemsets=[];
+		foreach($recent_problemsets as $problemset){
+			if($problemset->public || Gate::allows('view',$problemset)){
+				array_push($home_page_problemsets,$problemset);
+			}
+		}
+
+		return view('home',[
+			'home_page_problemsets' => $home_page_problemsets]);
 	}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,47 @@
 @extends ('layouts.master')
 
 @section ('title')
-Home
+{{ojoption('site_name')}}
 @endsection
 
 @section ('content')
-welcome wzoj!
+<div class="col-xs-9 row">
+  @foreach ($home_page_problemsets as $problemset)
+    <div class="col-xs-4">
+      <div class="thumbnail problemset-dock" data-href="/s/{{$problemset->id}}">
+        <div class="row">
+          <div class="col-xs-12">
+            <div style="padding: 9px;min-height: 130px;">
+	      <small class="pull-right">{{trans('wzoj.problem_type_'.$problemset->type)}}</small>
+              <div class="dock-heading">{{$problemset->name}}</div>
+	      @if ($problemset->type <> 'set')
+	      	{{$problemset->contest_start_at}} - <br>
+		{{$problemset->contest_end_at}} <br>
+	      @endif
+  		{!! Purifier::clean($problemset->description) !!}
+	    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endforeach
+</div>
+<div class="col-xs-3 row">
+  <div class="panel panel-wzoj">
+    <div class="panel-heading">{{trans('wzoj.notices')}}</div>
+    <div class="panel-body">
+	{{trans('wzoj.none')}}
+    </div>
+  </div>
+</div>
+@endsection
+
+@section ('scripts')
+<script>
+jQuery(document).ready(function($) {
+	$(".problemset-dock").click(function() {
+		window.document.location = $(this).data("href");
+	});
+});
+</script>
 @endsection
