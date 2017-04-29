02189d50f605f692d35317f81d3e3046074aaa92
Massimo Dong <dongmassimo@gmail.com>
2017-04-29 18:24:42 +0800

    no translate

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="google" content="notranslate" />
     <link rel="icon" href="/favicon.ico">
 
     <title>@yield('title')</title>
