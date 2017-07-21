0502e3477a8abd28b2b97074b41d14ee2292c7c0
Massimo Dong <dongmassimo@gmail.com>
2017-07-21 12:13:19 +0200

    fileManager: download files

--- app/FileManager.php
+++ app/FileManager.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Storage;
+use ZipArchive;
 
 class FileManager
 {
@@ -19,8 +20,41 @@ class FileManager
 		if(empty($dir_names)) return '/';
 		else return '/'.join('/', $dir_names).'/';
 	}
+	static function downloadFiles($disk, $basePath, $userPath, $files,$name){
+		$zip = new ZipArchive;
+		$zip_name = $name.'.zip';
+		$zip_file = tempnam("","");
+		$zip->open($zip_file, ZipArchive::CREATE);
+
+		foreach($files as $file){
+			$file = basename($file);
+			if(in_array($basePath.$userPath.$file,Storage::disk($disk)->directories($basePath.$userPath), true)){
+				//zip all files in directory
+				$pre_length = strlen(FileManager::resolvePath($basePath.$userPath))-1;
+				foreach(Storage::disk($disk)->allFiles($basePath.$userPath.$file) as $sub_file){
+					$zip->addFromString(substr($sub_file, $pre_length), Storage::disk($disk)->get($sub_file));
+				}
+			}else if(Storage::disk($disk)->has($basePath.$userPath.$file)){
+				//zip file
+				$zip->addFromString($file, Storage::disk($disk)->get($basePath.$userPath.$file));
+			}
+		}
+		$zip->close();
+		return response()->download($zip_file, $zip_name)->deleteFileAfterSend(true);
+	}
 	static function getRequests($request, $config){
-		if(isset($request->file)){
+		if(isset($request->action)){
+			switch($request->action){
+				case 'download':
+					return FileManager::downloadFiles($config['disk'],
+									  $config['basepath'],
+									  FileManager::resolvePath($request->path),
+									  $request->id,
+									  $config['title']);
+				default:
+					abort(403);
+			}
+		}else if(isset($request->file)){
 			$userFile = FileManager::resolvePath($request->file);
 			if(Storage::disk($config['disk'])->has($config['basepath'].$userFile)){
 				return view('fileManager.readText', [
--- resources/views/fileManager/index.blade.php
+++ resources/views/fileManager/index.blade.php
@@ -5,10 +5,26 @@
 @endsection
 
 @section ('content')
-<div style="padding:10px;">{{trans('wzoj.cur_path')}}:{{$userPath}} <a href="#" onclick="transit('..');return false;">{{trans('wzoj.back')}}</a></div>
+<div style="padding:10px;" class="col-xs-12">{{trans('wzoj.cur_path')}}:{{$userPath}} <a href="#" onclick="transit('..');return false;">{{trans('wzoj.back')}}</a></div>
+
+<div class="col-xs-6">file upload</div>
+<form id="fileManager_form" class="form-inline col-xs-1"></form>
+<div class="col-xs-5">
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    {{trans('wzoj.operations')}}
+    <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+      <li><a href="#" onclick="fileManager_action('download');return false;">{{trans('wzoj.download')}}</a></li>
+    </ul>
+  </div>
+</div>
+
 <table id="fileManagerTable" class="table table-striped">
 <thead>
   <th style="width: 1%"><input name="select_all" value="1" type="checkbox"></th>
+  <th style="display:none">{{trans('wzoj.filename')}}</th>
   <th>{{trans('wzoj.filename')}}</th>
   <th style="width: 20%">{{trans('wzoj.filesize')}}</th>
   <th style="width: 20%">{{trans('wzoj.fileLastModified')}}</th>
@@ -17,6 +33,7 @@
   @foreach ($directories as $dir)
   <tr>
     <td></td>
+    <td style="display:none">{{pathinfo($dir)['basename']}}</td>
     <td><a href="#" onclick="transit('{{pathinfo($dir)['basename']}}');return false;">{{pathinfo($dir)['basename']}}/</a></td>
     <td></td>
     <td></td>
@@ -25,6 +42,7 @@
   @foreach ($files as $file)
   <tr>
     <td></td>
+    <td style="display:none">{{pathinfo($file)['basename']}}</td>
     <td><span style="cursor:pointer" onclick="preview('{{pathinfo($file)['basename']}}')">{{pathinfo($file)['basename']}}</span></td>
     <td>{{Storage::disk($config['disk'])->size($file)}}</td>
     <td>{{date('M d H:i', Storage::disk($config['disk'])->lastModified($file))}}</td>
@@ -57,6 +75,16 @@ function preview(path){
 	setTimeout('div_on_preview = true', 1000);
 }
 
+function fileManager_action( action ){
+	if(file_names.length == 0) return;
+	$('#fileManager_form').html('');
+	$('#fileManager_form').append('<input hidden name="action" value="' + action + '">');
+	$('#fileManager_form').append("<input hidden name='path' value='" + "{{$userPath}}" + "'></input>");
+	var submitInput = $("<input style='display:none' type='submit' />");
+	$("#fileManager_form").append(submitInput);
+        submitInput.trigger("click");
+}
+
 $(document).click(function(){
 	if(div_on_preview){
 		$('#preview_div').hide();
@@ -66,7 +94,7 @@ $(document).click(function(){
 
 var file_names = [];
 jQuery(document).ready(function($){
-	createDatatableWithCheckboxs("fileManagerTable", file_names, "");
+	createDatatableWithCheckboxs("fileManagerTable", file_names, "fileManager_form");
 });
 </script>
 @endsection
