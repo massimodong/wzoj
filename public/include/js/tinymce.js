44f3c2d210f8b4853c2d9cd6b9c2d987342e088a
Massimo Dong <dongmassimo@gmail.com>
2017-08-25 20:08:11 +0800

    change diy page admin ui

--- public/include/js/tinymce.js
+++ public/include/js/tinymce.js
@@ -96,6 +96,58 @@ tinymce.init({
 });
 
 tinymce.init({
+	    selector: '.ojeditor_inline',
+	    inline: true,
+	    language_url: '/include/js/tinyMCE/zh_CN.js',
+	    theme: 'modern',
+	    plugins: 'image imagetools paste autolink autosave code codesample textcolor contextmenu link lists media preview searchreplace colorpicker table',
+	    menubar:false,
+	    toolbar: "undo redo | styleselect formatselect fontselect fontsizeselect | forecolor bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | link image media codesample table | bullist numlist outdent indent | removeformat | code searchreplace | preview | newdocument ",
+	    contextmenu: "link image inserttable | cell row column deletetable | paste",
+	    paste_data_images: true,
+	    extended_valid_elements : 'img[class=img-responsive|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style]',
+	    imagetools_cors_hosts: [''],
+	    setup: function (editor) {
+		    editor.on('change', function () {
+			    tinymce.triggerSave();
+		    });
+	    },
+	    images_upload_handler: function (blobInfo, success, failure) {
+
+	    var xhr, formData;
+
+	    xhr = new XMLHttpRequest();
+	    xhr.withCredentials = false;
+	    xhr.open('POST', '/files');
+
+	    xhr.onload = function() {
+		    var json;
+
+		    if (xhr.status != 200) {
+			    failure('HTTP Error: ' + xhr.status);
+			    return;
+		    }
+
+		    json = JSON.parse(xhr.responseText);
+
+		    if (!json || typeof json.location != 'string') {
+			    failure('Invalid JSON: ' + xhr.responseText);
+			    return;
+		    }
+
+		    success(json.location);
+	    };
+
+	    formData = new FormData();
+	    formData.append('file', blobInfo.blob(), blobInfo.filename());
+	    formData.append('_token',csrf_token);
+
+	    xhr.send(formData);
+	    }
+});
+
+
+tinymce.init({
 	    selector: '.posteditor_inline',
 	    inline: true,
 	    language_url: '/include/js/tinyMCE/zh_CN.js',
--- resources/views/admin/diy_page_edit.blade.php
+++ resources/views/admin/diy_page_edit.blade.php
@@ -1,4 +1,4 @@
-@extends ('admin.layout')
+@extends ('layouts.master')
 
 @section ('title')
 {{trans('wzoj.diy_pages')}}
@@ -13,23 +13,18 @@
 <form method="POST" class="form-horizontal" id="diyPage_form">
   {{csrf_field()}}
   {{method_field('PUT')}}
-  <div class="form-group">
+  <div class="form-group col-xs-6">
     <label class="control-label col-sm-2" for="name">{{trans('wzoj.name')}}</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="name" name="name" value="{{$diyPage->name}}">
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group col-xs-6">
     <label class="control-label col-sm-2" for="url">{{trans('wzoj.url')}}</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="url" name="url" value="{{$diyPage->url}}">
     </div>
   </div>
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="url">{{trans('wzoj.content')}}</label>
-    <div class="col-sm-10">
-      <textarea class="ojeditor" id="content" name="content">{{$diyPage->content}}</textarea>
-    </div>
-  </div>
+  <div class="ojeditor_inline col-xs-12" style="border: 1px dotted black;" id="content">{!! $diyPage->content !!}</div>
 </form>
 @endsection
