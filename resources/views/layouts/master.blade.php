e7cd334d8f09106e95013434162b766af7567e55
Massimo Dong <dongmassimo@gmail.com>
2018-03-03 16:49:52 +0800

    show logo only if set

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,7 +47,9 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="/"><img src="{{ojoption('logo_url')}}" class="navbar-logo" width="50" height="50"></a>
+          @if (strlen(ojoption('logo_url')))
+            <a href="/"><img src="{{ojoption('logo_url')}}" class="navbar-logo" width="50" height="50"></a>
+          @endif
           <a class="navbar-brand" href=
 	  @section ('home_href')
 	  "/"
