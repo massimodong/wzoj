3752bff89c95ffe28efee3450b65431391b3025e
sjx233 <sjx233@qq.com>
2019-04-22 16:44:34 +0800

    Use padding instead of constant width

--- database/migrations/2017_08_18_160611_init_sidebars_and_diy_pages.php
+++ database/migrations/2017_08_18_160611_init_sidebars_and_diy_pages.php
@@ -20,23 +20,23 @@ class InitSidebarsAndDiyPages extends Migration
 		    <p>WZMS ONLINE JUDGE旨在提供一套简单易用的校内OJ系统,实现日常的训练、作业、比赛等功能。</p>
 		    <h3>我提交的程序是怎样运行的？</h3>
 		    <p>后台运行的<a href="https://github.com/massimodong/wzoj-judger" target="blank">评测机</a>执行程序的编译、运行、评判任务。评测机以及提交的程序都在Linux环境中运行，部分语言的编译以及执行参数如下:</p>
-		    <table style="width: 712.333px;" border="1">
+		    <table border="1">
 		    <tbody>
 		    <tr>
-		    <td style="width: 48px;">C</td>
-		    <td style="width: 649.333px;">gcc Main.c -o Main -fno-asm -Wall -lm --static -std=c99 -DONLINE_JUDGE</td>
+		    <td style="padding: 0 6px;">C</td>
+		    <td style="padding: 0 6px;">gcc Main.c -o Main -fno-asm -Wall -lm --static -std=c99 -DONLINE_JUDGE</td>
 		    </tr>
 		    <tr>
-		    <td style="width: 48px;">C++</td>
-		    <td style="width: 649.333px;">g++ Main.cc -o Main -fno-asm -Wall -lm --static -std=c++0x -DONLINE_JUDGE</td>
+		    <td style="padding: 0 6px;">C++</td>
+		    <td style="padding: 0 6px;">g++ Main.cc -o Main -fno-asm -Wall -lm --static -std=c++0x -DONLINE_JUDGE</td>
 		    </tr>
 		    <tr>
-		    <td style="width: 48px;">Pascal</td>
-		    <td style="width: 649.333px;">fpc Main.pas -Cs32000000 -Sh -O2 -Co -Ct -Ci</td>
+		    <td style="padding: 0 6px;">Pascal</td>
+		    <td style="padding: 0 6px;">fpc Main.pas -Cs32000000 -Sh -O2 -Co -Ct -Ci</td>
 		    </tr>
 		    <tr>
-		    <td style="width: 48px;">Python</td>
-		    <td style="width: 649.333px;">python3.* Main.py</td>
+		    <td style="padding: 0 6px;">Python</td>
+		    <td style="padding: 0 6px;">python3.* Main.py</td>
 		    </tr>
 		    </tbody>
 		    </table>
