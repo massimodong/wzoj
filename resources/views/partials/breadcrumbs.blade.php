b1b1d95a988a7e699d2a3fb03b4a69bab5108c83
Massimo Dong <dongmassimo@gmail.com>
2019-07-03 16:37:58 +0800

    breadcrumbs

---
+++ resources/views/partials/breadcrumbs.blade.php
@@ -0,0 +1,13 @@
+@if ($breadcrumbs)
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+    @foreach ($breadcrumbs as $breadcrumb)
+      @if (!$breadcrumb->last)
+        <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+      @else
+        <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb->title }}</li>
+      @endif
+    @endforeach
+    </ol>
+  </nav>
+@endif
