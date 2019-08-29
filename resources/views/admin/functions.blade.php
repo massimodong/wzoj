fe06ca4e58aba0579c86c796ce797eb6835eddc3
Massimo Dong <dongmassimo@gmail.com>
2019-08-29 18:28:26 +0800

    admin UI

--- resources/views/admin/functions.blade.php
+++ resources/views/admin/functions.blade.php
@@ -5,28 +5,22 @@
 @endsection
 
 @section ('content')
-  <div class="col-xs-12">
-    <form action="/admin/functions/broadcast" method="POST" class="form-horizontal">
-      {{csrf_field()}}
-      <h3>{{trans('wzoj.broadcast')}}</h3>
-      <div class="form-group">
-        <label for="title" class="col-sm-2 control-label">{{trans('wzoj.title')}}:</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="title" name="title" value="">
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="content" class="col-sm-2 control-label">{{trans('wzoj.content')}}:</label>
-        <div class="col-sm-10">
-	  <textarea class="form-control" id="content" name="content" rows="5"></textarea>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default">{{trans('wzoj.submit')}}</button>
-        </div>
-      </div>
-    </form>
-    <hr>
+<h3>{{trans('wzoj.broadcast')}}</h3>
+<form action="/admin/functions/broadcast" method="POST">
+  {{csrf_field()}}
+  <div class="form-group row">
+    <label for="title" class="col-sm-2 col-form-label">{{trans('wzoj.title')}}:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="title" name="title" value="">
+    </div>
   </div>
+  <div class="form-group row">
+    <label for="content" class="col-sm-2 col-form-label">{{trans('wzoj.content')}}:</label>
+    <div class="col-sm-10">
+      <textarea class="form-control" id="content" name="content" rows="5"></textarea>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">{{trans('wzoj.submit')}}</button>
+</form>
+<hr>
 @endsection
--- resources/views/admin/side_panel_edit.blade.php
+++ resources/views/admin/side_panel_edit.blade.php
@@ -10,28 +10,29 @@
 @endsection
 
 @section ('content')
-<form method="POST" class="form-horizontal" id="sidePanel_form">
+<form method="POST">
   {{csrf_field()}}
   {{method_field('PUT')}}
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="title">{{trans('wzoj.title')}}</label>
-    <div class="col-xs-10">
+  <div class="form-group row">
+    <label class="col-form-label col-sm-2" for="title">{{trans('wzoj.title')}}</label>
+    <div class="col-sm-10">
       <input type="text" class="form-control" id="title" name="title" value="{{$sidePanel->title}}">
     </div>
   </div>
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="index">{{trans('wzoj.index')}}</label>
-    <div class="col-xs-10">
+  <div class="form-group row">
+    <label class="col-form-label col-sm-2" for="index">{{trans('wzoj.index')}}</label>
+    <div class="col-sm-10">
       <input aria-describedby="helpBlockPanelIndex" type="text" class="form-control" id="index" name="index" value="{{$sidePanel->index}}">
       <span id="helpBlockPanelIndex" class="help-block">{{trans('wzoj.msg_panel_index_help')}}</span>
     </div>
   </div>
-  <div class="form-group">
-    <label for="content" class="col-xs-2 control-label"> {{trans('wzoj.content')}} </label>
-    <div class="col-xs-10">
+  <div class="form-group row">
+    <label for="content" class="col-sm-2 col-form-label"> {{trans('wzoj.content')}} </label>
+    <div class="col-sm-10">
       <textarea class='ojeditor' id='content' name='content'>{{htmlspecialchars($sidePanel->content)}}</textarea>
     </div>
   </div>
+  <button type="submit" class="btn btn-primary">{{trans('wzoj.submit')}}</button>
 
 </form>
 @endsection
