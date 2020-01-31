d2a85e701bf771341cde28cc84f17a548431dcf4
Massimo Dong <dongmassimo@gmail.com>
2020-01-31 19:36:28 +0800

    view contest

--- resources/views/problemsets/view_oi.blade.php
+++ resources/views/problemsets/view_oi.blade.php
@@ -4,50 +4,44 @@
 {{$problemset->name}}
 @endsection
 
-@include ('layouts.contest_header')
-
 @section ('content')
 {!! Breadcrumbs::render('problemset', $problemset) !!}
 <h1 class='page-header text-center'>{{$problemset->name}}</h1>
+<h6 class='text-center'><span style="color:green">{{$problemset->contest_start_at}}</span> - <span style="color:red">{{$problemset->contest_end_at}}</span></h6>
 @can ('update', $problemset)
 <div class="pull-right"><a href="/s/{{$problemset->id}}/edit">{{trans('wzoj.edit')}}</a></div>
 @endif
 
-<div id="home" class="tab-pane fade in active">
-  {!! $problemset->description !!}
-  <p>
-  <span style="color:#00CC00">{{trans('wzoj.contest_start_at')}}:{{$problemset->contest_start_at}}</span><br>
-  <span style="color:#FF0000">{{trans('wzoj.contest_end_at')}}:{{$problemset->contest_end_at}}</span>
-  </p>
+{!! $problemset->description !!}
 
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th style='width:5%'></th>
-      <th style='width:5%'>{{trans('wzoj.index')}}</th>
-      <th class="text-left">{{trans('wzoj.name')}}</th>
-      <th style='width:13%'>{{trans('wzoj.source')}}</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($problems as $problem)
-    <tr>
-      <td>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th></th>
+        <th>{{trans('wzoj.index')}}</th>
+        <th>{{trans('wzoj.name')}}</th>
+        <th>{{trans('wzoj.source')}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($problems as $problem)
+      <tr>
+        <td>
         @if (isset($max_scores[$problem->id]) && $max_scores[$problem->id] >= 100)
           <span class="glyphicon glyphicon-ok" style="color:green"></span>
         @elseif (isset($max_scores[$problem->id]) && $max_scores[$problem->id] >= 0)
           <span style="color:red">
-          {{$max_scores[$problem->id]}}</span>
+         {{$max_scores[$problem->id]}}</span>
         @endif
       </td>
       <td>{{$problem->pivot->index}}</td>
       <td class="text-left"><a href='/s/{{$problemset->id}}/{{$problem->id}}'>{{$problem->name}}</a></td>
       <td>{{$problem->source}}</td>
-    </tr>
-    @endforeach
-  </tbody>
+      </tr>
+      @endforeach
+    </tbody>
   </table>
 </div>
-<!-- home -->
 
 @endsection
