c3a273ef096f70d2e9de53eec514665754784b4e
Massimo Dong <dongmassimo@gmail.com>
2020-02-09 12:26:41 +0800

    fix bugs

--- resources/views/problemsets/contests.blade.php
+++ resources/views/problemsets/contests.blade.php
@@ -7,13 +7,6 @@
 @section ('content')
 {!! Breadcrumbs::render('contests') !!}
 
-@can ('create',App\Problemset::class)
-<form method='POST'>
-    {{csrf_field()}}
-    <button type="submit" class="btn btn-default">+</button>
-</form>
-@endcan
-
 <div class="table-responsive">
   <table class="table">
   <thead>
