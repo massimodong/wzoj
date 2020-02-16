aa4a01feef3cf0226ab775171d6205827215e76b
Massimo Dong <dongmassimo@gmail.com>
2020-02-16 20:47:42 +0800

    user profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -14,7 +14,7 @@
   <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="configModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="configModalLabel">{{trans('wzoj.edit_profile')}}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
