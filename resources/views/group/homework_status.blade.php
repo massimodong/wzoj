abda34286e1707f0e89e8e6819749d8780303926
Massimo Dong <dongmassimo@gmail.com>
2021-02-07 16:38:35 +0800

    change ui of ranklist

--- resources/views/group/homework_status.blade.php
+++ resources/views/group/homework_status.blade.php
@@ -11,41 +11,39 @@
 
 @section ('content')
 
-<div class="col-xs-12">
-  <table id="homework-status-table" class="table">
-    <thead>
-      <tr>
-        <th style="display: none"></th>
-        <th style="width: 8%">{{trans('wzoj.username')}}</th>
-        <th style="width: 8%">{{trans('wzoj.fullname')}}</th>
-        <th style="width: 9%">x/{{$total_score}}</th>
-	@foreach ($problems as $problem)
-	  <th><a href="/s/{{$problem->pivot->problemset_id}}/{{$problem->id}}" style="color:black">{{$problem->name}}</a></th>
-	@endforeach
-      </tr>
-    </thead>
-    <tbody>
-    @foreach ($group->users as $user)
-      <tr @if (Auth::check() && ($user->id == Auth::user()->id)) class="table-primary" @endif>
-        <td style="display: none">@if (Auth::check() && ($user->id == Auth::user()->id)) 1 @endif</td>
-        <td><a href="/users/{{$user->id}}">{{$user->name}}</a></td>
-	<td>{{$user->fullname}}</td>
-	<td>{{$user_total_scores[$user->id]}}</td>
-	@foreach ($problems as $problem)
-	  <td>
-	  @if (($score = ($user_max_scores[$user->id][$problem->pivot->problemset_id][$problem->id])) >= 100)
-	    <a href="/solutions?problemset_id={{$problem->pivot->problemset_id}}&user_name={{$user->name}}&problem_id={{$problem->id}}"><span style="color: green;">{{$score}}</span></a>
-	  @elseif ($score >= 0)
-	    <a href="/solutions?problemset_id={{$problem->pivot->problemset_id}}&user_name={{$user->name}}&problem_id={{$problem->id}}"><span style="color: red;">{{$score}}</span></a>
-	  @else
-	    <span style="color: grey">0</span>
-	  @endif
-	  </td>
-	@endforeach
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
+<div class="table-responsive">
+<table id="homework-status-table" class="table ranklist-table">
+  <thead>
+    <tr>
+      <th style="display: none"></th>
+      <th>{{trans('wzoj.user')}}</th>
+      <th>x/{{$total_score}}</th>
+      @foreach ($problems as $problem)
+        <th><a href="/s/{{$problem->pivot->problemset_id}}/{{$problem->id}}" style="color:black">{{$problem->name}}</a></th>
+      @endforeach
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($group->users as $user)
+    <tr @if (Auth::check() && ($user->id == Auth::user()->id)) class="table-primary" @endif>
+      <td style="display: none">@if (Auth::check() && ($user->id == Auth::user()->id)) 1 @endif</td>
+      <td>@include ('partials.user_badge', ['user' => $user])</td>
+      <td>{{$user_total_scores[$user->id]}}</td>
+      @foreach ($problems as $problem)
+        <td>
+          @if (($score = ($user_max_scores[$user->id][$problem->pivot->problemset_id][$problem->id])) >= 100)
+            <a href="/solutions?problemset_id={{$problem->pivot->problemset_id}}&user_name={{$user->name}}&problem_id={{$problem->id}}"><span style="color: green;">{{$score}}</span></a>
+          @elseif ($score >= 0)
+            <a href="/solutions?problemset_id={{$problem->pivot->problemset_id}}&user_name={{$user->name}}&problem_id={{$problem->id}}"><span style="color: red;">{{$score}}</span></a>
+          @else
+            <span style="color: grey">0</span>
+          @endif
+        </td>
+      @endforeach
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 </div>
 @endsection
 
@@ -56,7 +54,7 @@ $('#homework-status-table').DataTable({
 	searching: false,
 	paging: false,
 	bInfo: false,
-	order: [[0, "desc"], [3, "desc"]],
+	order: [[0, "desc"], [2, "desc"]],
 });
 </script>
 @endsection
