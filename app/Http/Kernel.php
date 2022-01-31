50c7ccf7377b695259e4cdc6f77a8d1023b8e34c
Massimo Dong <dongmassimo@gmail.com>
2022-01-31 12:09:35 +0800

    trusted proxies

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
