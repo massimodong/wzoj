952ef616bd041a6eb5c1ddf2817105044896f744
Dong Massimo <ms@maxmute.com>
2023-10-22 16:34:20 +0800

    change maintainance message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>维护中，马上回来。</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">维护中，马上回来。</div>
             </div>
         </div>
     </body>
