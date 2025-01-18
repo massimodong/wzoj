e5e80a45cd18357ddda1a4bb69da128691143e2d
Dong Massimo <ms@maxmute.com>
2025-01-18 20:13:42 +0800

    update .env.example for pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,9 +36,9 @@ return [
             'options' => [
               'cluster' => env('PUSHER_APP_CLUSTER'),
               'encrypted' => true,
-              'host' => '127.0.0.1',
-              'port' => 6001,
-              'scheme' => 'http'
+              'host' => env('PUSHER_HOST', '127.0.0.1'),
+              'port' => env('PUSHER_PORT', 6001),
+              'scheme' => env('PUSHER_SCHEME', 'http')
             ],
         ],
 
