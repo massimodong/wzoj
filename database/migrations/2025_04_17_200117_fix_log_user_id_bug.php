b293d17a87c1a8f3ca34a0bc3e2458ba0af92b92
Massimo Dong <dongmassimo@gmail.com>
2025-04-17 08:35:35 -0400

    fix log bugs

---
+++ database/migrations/2025_04_17_200117_fix_log_user_id_bug.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\UserLog;
+
+class FixLogUserIdBug extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      UserLog::chunk(100, function($userlogs){
+        foreach ($userlogs as $userlog){
+          switch($userlog->action_name){
+            case "registered":
+              if(array_key_exists("id", $userlog->action_payload)){
+                $userlog->user_id = $userlog->action_payload["id"];
+                $userlog->save();
+              }
+              break;
+            case "failed_login":
+              $user = \App\User::where('name', $userlog->action_payload["name"])->first();
+              if(!is_null($user)){
+                $userlog->user_id = $user->id;
+                $userlog->save();
+              }
+              break;
+            case "password_reset":
+              if(array_key_exists("id", $userlog->action_payload)){
+                $userlog->user_id = $userlog->action_payload["id"];
+                $userlog->save();
+              }
+              break;
+          }
+        }
+      });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
