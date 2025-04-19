2c4bccca3c013196e97150000dc9fd42b6d85262
Massimo Dong <dongmassimo@gmail.com>
2025-04-18 22:32:49 -0400

    hide badge of non-display user

--- resources/views/partials/user_badge_nickname.blade.php
+++ resources/views/partials/user_badge_nickname.blade.php
@@ -1,3 +1,9 @@
+@if ($user->is_display())
 <div style="display: inline">
   <a class="user-badge badge badge-light" data-toggle="popover" data-uid="{{$user->id}}" data-nickname="{{$user->nickname}}" data-uname="{{$user->name}}" data-avatarurl="{{$user->avatar_url('md')}}" data-description="{{$user->description}}" href="/users/{{$user->id}}">{{$user->shortname(6)}}</a>
 </div>
+@else
+<div style="display: inline">
+  <a class="user-badge badge badge-light" data-toggle="popover" data-uid="{{$user->id}}" data-nickname="{{$user->name}}" data-uname="{{$user->name}}" data-avatarurl="{{$user->avatar_url('md')}}" data-description="" href="/users/{{$user->id}}">{{mb_substr($user->name, 0, 6)}}</a>
+</div>
+@endif
